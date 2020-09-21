67e9465f8ec6aa029412a9a9605bf751a3fbee6f
iseunghan <tmdgks301@naver.com>
2020-09-22 01:19:27 +0900

    가장 큰 수 풀이

--- src/me/iseunghan/Main.java
+++ src/me/iseunghan/Main.java
@@ -3,9 +3,15 @@ package me.iseunghan;
 public class Main {
 
     public static void main(String[] args) {
-        int[] citations = {6,6,6,6,6,1};
+        /*int[] citations = {6,6,6,6,6,1};
         H_index hIndexSolution = new H_index();
         int result = hIndexSolution.run(citations);
+        System.out.println("result = " + result);*/
+
+        int[] numbers = {3, 30, 34, 5, 9};
+        Max_number max_number = new Max_number();
+        String result = max_number.run(numbers);
         System.out.println("result = " + result);
+
     }
 }
---
+++ src/me/iseunghan/Max_number.java
@@ -0,0 +1,29 @@
+package me.iseunghan;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Max_number {
+    public String run(int[] numbers) {
+        // int[] numbers = {6, 10, 2}
+        String result = "";
+        String[] str = new String[numbers.length];
+        // int[] to string[]
+        for (int i = 0; i < numbers.length; i++) {
+            str[i] = numbers[i] + "";
+        }
+
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            }
+        });
+
+        for (int i = 0; i < str.length; i++) {
+            result += str[i];
+        }
+        return result;
+
+    }
+}
