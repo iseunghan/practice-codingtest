9d871eb477ba628c650225e6fa6e8f73ce1793fa
iseunghan <tmdgks301@naver.com>
2020-10-12 23:34:11 +0900

    백준 숫자의 합 11720

---
+++ src/me/iseunghan/baekjoon/SumOfNum_11720.java
@@ -0,0 +1,19 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+
+public class SumOfNum_11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int answer = 0;
+        int length = Integer.parseInt(bf.readLine());
+        String numbers = bf.readLine();
+
+        for(int i=0; i<length;i++){
+            answer += Integer.parseInt(String.valueOf(numbers.charAt(i)));
+        }
+
+        System.out.println(answer);
+    }
+}
