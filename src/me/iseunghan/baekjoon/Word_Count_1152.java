7d15479c194b642923ee645febdcf390c8730d50
iseunghan <tmdgks301@naver.com>
2020-10-13 22:38:08 +0900

    단어의 개수_1152

---
+++ src/me/iseunghan/baekjoon/Word_Count_1152.java
@@ -0,0 +1,19 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Word_Count_1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+//        st = new StringTokenizer(bf.readLine());
+//        bw.write(st.countTokens() + ""); // Token의 개수가 곧 정답이다.
+        String[] arr = bf.readLine().split(" ");
+        bw.write(arr.length + "");
+        bw.flush();
+        bw.close();
+    }
+}
