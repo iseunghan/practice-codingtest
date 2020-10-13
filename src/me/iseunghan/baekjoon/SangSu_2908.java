d6384969f428d0a17b5ffa636f7e32708056950a
iseunghan <tmdgks301@naver.com>
2020-10-13 23:16:43 +0900

    상수 2908번

---
+++ src/me/iseunghan/baekjoon/SangSu_2908.java
@@ -0,0 +1,29 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class SangSu_2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int MAX = 0;
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < 2; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            // 3자리수를 자리 바꿈
+            int d = num % 10;
+            int dd = (num / 10) % 10;
+            int ddd = num / 100;
+            int newNum = d * 100 + dd * 10 + ddd;
+            if (MAX < newNum) {
+                MAX = newNum;
+            }
+        }
+        bw.write(MAX + "");
+        bw.flush();
+        bw.close();
+
+    }
+}
