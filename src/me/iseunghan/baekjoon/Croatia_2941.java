71afd74ee61137a269a1a92595a3dcdcb65addf0
iseunghan <tmdgks301@naver.com>
2020-10-14 22:23:18 +0900

    크로아티아 알파벳 2941번 풀이

---
+++ src/me/iseunghan/baekjoon/Croatia_2941.java
@@ -0,0 +1,23 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+
+public class Croatia_2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] croatians = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        String alphabet = bf.readLine();
+        int answer = 0;
+
+        for (int i = 0; i < croatians.length; i++) {
+            if (alphabet.contains(croatians[i])) { // 크로아티아 알파벳이 포함?
+//                answer++;
+                alphabet = alphabet.replace(croatians[i], "/");
+            }
+        }
+//        String s = alphabet.replaceAll("/", "");
+        answer += alphabet.length();
+        System.out.println(answer);
+    }
+}
