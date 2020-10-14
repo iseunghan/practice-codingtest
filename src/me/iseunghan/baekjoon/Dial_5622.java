19b4cd4f6581a98da9fcca96a6dba98edd0f0328
iseunghan <tmdgks301@naver.com>
2020-10-14 22:11:30 +0900

    다이얼 5622번 풀이

---
+++ src/me/iseunghan/baekjoon/Dial_5622.java
@@ -0,0 +1,22 @@
+package me.iseunghan.baekjoon;
+
+import java.util.Scanner;
+
+public class Dial_5622 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 65;
+            if (idx == 18 || idx == 21 || (idx / 3) == 8) { //S , Z인 경우
+                answer += idx / 3 + 2;
+            }else{
+                answer += idx / 3 + 3;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
