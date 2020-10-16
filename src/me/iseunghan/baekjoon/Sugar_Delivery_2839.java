b1e5d05e80295a2383200b68de4ff0d00a7fc8c3
iseunghan <tmdgks301@naver.com>
2020-10-17 00:06:47 +0900

    설탕배달 2839번 풀이

---
+++ src/me/iseunghan/baekjoon/Sugar_Delivery_2839.java
@@ -0,0 +1,27 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+
+public class Sugar_Delivery_2839 {
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        if (N == 4 || N == 7) {
+            bw.write("-1");
+        } else {
+            while (N % 5 != 0) {
+                N -= 3; answer += 1;
+
+            }
+            answer += N / 5;
+            bw.write(answer + "");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
