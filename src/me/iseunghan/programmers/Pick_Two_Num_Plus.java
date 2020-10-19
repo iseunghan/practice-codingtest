7571b1d55e226dd6388d8d6def40434ac2a0b055
iseunghan <tmdgks301@naver.com>
2020-10-19 22:45:37 +0900

    두수 뽑아서 더하기 풀이

---
+++ src/me/iseunghan/programmers/Pick_Two_Num_Plus.java
@@ -0,0 +1,27 @@
+package me.iseunghan.programmers;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Pick_Two_Num_Plus {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        answer = new int[set.size()];
+        int idx = 0;
+        for (Integer integer : set) {
+            answer[idx++] = integer;
+        }
+        Arrays.sort(answer);
+
+        return answer;
+    }
+}
