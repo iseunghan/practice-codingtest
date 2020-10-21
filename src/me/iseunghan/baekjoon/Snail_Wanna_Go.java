33b460acdc4a4a2ba4b4dc5676832c84a5e8736b
iseunghan <tmdgks301@naver.com>
2020-10-21 23:06:11 +0900

    달팽이는 올라가고 싶다 2869번 문제 풀이

---
+++ src/me/iseunghan/baekjoon/Snail_Wanna_Go.java
@@ -0,0 +1,28 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Snail_Wanna_Go {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        V -= A; // 첫날 값 미리 구해주기.
+        int up = A - B; // 실제 올라가는 값
+        int day = V / up;
+        if (V % up != 0) {
+            day++;
+        }
+        bw.write(day + 1 + "");
+
+        // resource manage
+        bw.flush();
+        bw.close();
+    }
+}
