8c03da5445048b99186f5d44d3ff5d46fb30028d
iseunghan <tmdgks301@naver.com>
2020-10-21 23:08:45 +0900

    ACM 호텔 10250번 문제 풀이

---
+++ src/me/iseunghan/baekjoon/ACM_Hotel_10250.java
@@ -0,0 +1,37 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class ACM_Hotel_10250 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(bf.readLine());
+
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int H = Integer.parseInt(st.nextToken()); //층
+            int W = Integer.parseInt(st.nextToken()); //호수
+            int N = Integer.parseInt(st.nextToken()); //몇번째
+
+            int floor = 0; // 층수
+            int hosu = N / H; // 호수
+            if (N % H != 0) {
+                floor = N % H;
+                hosu++;
+            } else {
+                floor = H;
+            }
+            if (hosu < 10) {
+                bw.write(floor + "0" + hosu + "\n");
+            } else {
+                bw.write(floor + "" + hosu + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
