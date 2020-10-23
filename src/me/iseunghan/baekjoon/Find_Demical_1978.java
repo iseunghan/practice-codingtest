d5ca35bb4323e89eeede9380ba5eceb3556b2c19
iseunghan <tmdgks301@naver.com>
2020-10-23 20:20:51 +0900

    소수 찾기1 1978번 문제 풀이

---
+++ src/me/iseunghan/baekjoon/Find_Demical_1978.java
@@ -0,0 +1,35 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Find_Demical_1978 {
+    static int[] demical = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        for (int i = 2; i < demical.length; i++) {
+            if (demical[i] == 0) {
+                for (int j = i * i; j < demical.length; j += i) {
+                    demical[j] = -1;
+                }
+            }
+        }
+
+        int N = Integer.parseInt(bf.readLine());
+        st = new StringTokenizer(bf.readLine());
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (a >= 2 && demical[a] != -1) {
+                answer++;
+            }
+        }
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+    }
+}
