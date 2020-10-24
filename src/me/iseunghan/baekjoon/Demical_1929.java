cfda451b178a7797cb8a508028c8f0e85204ae11
iseunghan <tmdgks301@naver.com>
2020-10-24 13:26:14 +0900

    소수 구하기 1929번 풀이

---
+++ src/me/iseunghan/baekjoon/Demical_1929.java
@@ -0,0 +1,37 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Demical_1929 {
+    static int[] demical;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        demical = new int[N + 1];
+        demical[0] = -1;
+        demical[1] = -1;
+        for (long i = 2; i < demical.length; i++) {
+            if (i <= 1000 && demical[(int) i] == 0) {
+                for (long j = i * i; j < demical.length; j += i) {
+                    demical[(int) j] = -1;
+                }
+            }
+        }
+
+        for (int i = M; i <= N; i++) {
+            if (demical[i] == 0) {
+                bw.write(i + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
