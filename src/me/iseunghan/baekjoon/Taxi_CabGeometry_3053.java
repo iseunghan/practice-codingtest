98c3388b6587f5f5d8bf258c2f621c8b30a2f24a
iseunghan <tmdgks301@naver.com>
2020-10-30 23:45:17 +0900

    택시 기하학 3053번 문제 풀이

---
+++ src/me/iseunghan/baekjoon/Taxi_CabGeometry_3053.java
@@ -0,0 +1,16 @@
+package me.iseunghan.baekjoon;
+
+import java.util.Scanner;
+
+public class Taxi_CabGeometry_3053 {
+    final static double PI = 3.14159265359;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int R = sc.nextInt();
+        double r1 = R * R * PI;
+        double r2 = R * R * 2;
+        System.out.printf("%6f" + "\n" + "%6f", r1, r2);
+    }
+}
