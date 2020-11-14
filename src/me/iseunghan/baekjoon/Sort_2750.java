e912378b9600e346a1d7da65f22dd9a1928c2456
iseunghan <tmdgks301@naver.com>
2020-11-15 00:00:44 +0900

    백준 2750번 수 정렬하기 풀이

---
+++ src/me/iseunghan/baekjoon/Sort_2750.java
@@ -0,0 +1,25 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Sort_2750 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(bf.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i : arr) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
