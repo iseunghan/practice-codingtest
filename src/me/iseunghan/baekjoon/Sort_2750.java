f783693a4020008ec5f83f955aeef8336fac7f4a
iseunghan <tmdgks301@naver.com>
2020-11-19 23:36:03 +0900

    백준 정렬 2750번 문제 풀이

--- src/me/iseunghan/baekjoon/Sort_2750.java
+++ src/me/iseunghan/baekjoon/Sort_2750.java
@@ -2,6 +2,7 @@ package me.iseunghan.baekjoon;
 
 import java.io.*;
 import java.util.Arrays;
+import java.util.Random;
 
 public class Sort_2750 {
     public static void main(String[] args) throws IOException {
@@ -9,16 +10,23 @@ public class Sort_2750 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(bf.readLine());
+        Random random = new Random(1000000);
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(bf.readLine());
+//            arr[i] = Integer.parseInt(bf.readLine());
+            arr[i] = N--;
         }
-
+        long start = System.currentTimeMillis();
         Arrays.sort(arr);
 
         for (int i : arr) {
             bw.write(i + "\n");
         }
+
+        bw.flush();
+        long end = System.currentTimeMillis();
+        double l = (end - start) / 1000.000;
+        bw.write(l + "");
         bw.flush();
         bw.close();
     }
