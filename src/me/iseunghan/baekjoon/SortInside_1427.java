1b56e5c205a73a3cf67f6cd603a723e176991e1d
iseunghan <tmdgks301@naver.com>
2020-11-29 23:16:49 +0900

    백준 1427번 솔트인사이드 풀이

---
+++ src/me/iseunghan/baekjoon/SortInside_1427.java
@@ -0,0 +1,20 @@
+package me.iseunghan.baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortInside_1427 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
+        String[] split = sc.nextLine().split("");
+
+        Arrays.sort(split);
+
+        for (int i = split.length-1; i >=0 ; i--) {
+            sb.append(split[i]);
+        }
+
+        System.out.println(sb.toString());
+    }
+}
