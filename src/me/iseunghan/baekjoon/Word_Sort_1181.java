f9d1688b3ab62a1773f26b9cc666b42cc46aae03
iseunghan <tmdgks301@naver.com>
2020-11-30 16:17:59 +0900

    백준1181번  단어정렬 문제 풀이

---
+++ src/me/iseunghan/baekjoon/Word_Sort_1181.java
@@ -0,0 +1,45 @@
+package me.iseunghan.baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class Word_Sort_1181 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(bf.readLine());
+
+        String[] arr = new String[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = bf.readLine();
+        }
+
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+
+
+        Stream<String> stream = Arrays.stream(arr).distinct();
+        stream.forEach(e ->{
+            try {
+                bw.write(e + "\n");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        });
+
+        bw.flush();
+        bw.close();
+    }
+}
