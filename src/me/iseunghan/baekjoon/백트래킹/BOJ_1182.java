d430debe9b1cd7c6e8ae3f2d8910e95ec715e70e
iseunghan <tmdgks301@naver.com>
2021-01-04 22:33:25 +0900

    BOJ[백트래킹] 1182번 - 부분수열의합(수정)

--- src/me/iseunghan/baekjoon/백트래킹/BOJ_1182.java
+++ src/me/iseunghan/baekjoon/백트래킹/BOJ_1182.java
@@ -30,23 +30,20 @@ public class BOJ_1182 {
     }
 
     public static void dfs(int depth, int num) {
-        if (num == S) {
-            result++;
-            return;
-        }
+        /*  depth가 N일 때만 체크 */
         if (depth == N) {
+            if (num == S) {
+                result++;
+            }
             return;
         }
+        /**
+         * ex) 3개의 수열 {1,2,3}
+         * 모든 부분수열 : {(공집합)}, {1}, {2}, {3}, {1,2}, {1,3}, {2,3}
+         * S = 0 일때는 공집합의 개수를 빼줘야 한다!
+         */
+            dfs(depth + 1, num);                    // 현재 인덱스(depth)를 더하지 않을 경우
+            dfs(depth + 1, num + arr[depth]);  // 현재 인덱스(depth)를 더하는 경우
 
-
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                dfs(depth + 1, num + arr[i]);
-                for (int j = i + 1; j < N; j++) {
-                    visit[j] = false;
-                }
-            }
-        }
     }
 }
