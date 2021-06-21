4bedc8ac92d9f3ad0132b8b6b24e1bae33c29f18
iseunghan <tmdgks301@naver.com>
2021-06-21 23:34:34 +0900

    프로그래머스 [정렬] : H-Index (JAVA) 문제 풀이

---
+++ src/me/iseunghan/programmers/정렬/H_Index.java
@@ -0,0 +1,24 @@
+package me.iseunghan.programmers.정렬;
+
+import java.util.*;
+
+public class H_Index {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int solution(int[] citations) {
+            int[] answer = new int[citations.length];
+
+            Arrays.sort(citations);
+
+            for (int i = citations.length - 1; i >= 0; i--) {
+                answer[i] = Math.min(citations[i], citations.length - i);
+            }
+
+            Arrays.sort(answer);
+            return answer[answer.length-1];
+        }
+    }
+}
